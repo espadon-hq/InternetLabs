5928e8c388afcf04a34a92ec3fb9743353e6cd1e
espadon-hq <vitashi02@gmail.com>
2025-03-01 14:51:47 +0200

    js3-lab done

---
+++ js3/script.js
@@ -0,0 +1,11 @@
+const printName = (name) => {
+  for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+  }
+};
+
+const surname = "Shimanskyi";
+const firstName = "Vitaliy";
+
+printName(surname);
+printName(firstName);
